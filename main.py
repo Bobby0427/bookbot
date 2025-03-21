2aec408bc7815a730eeeab731b7c13ca8a85f338
Bobby0427 <kenseb427@gmail.com>
2025-03-21 09:02:25 +0800

    improved outer appearance and more books added

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
-def get_book_text(book):
-    with open(book) as f:
-        file_contents = f.read()
-    return file_contents
+import sys
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
 
 
 def main():
-    file_path = "books/frankenstein.txt"
-   
-    text = get_book_text(file_path)
-    number = number_of_words(text)
-    print(f"{number} words found in the document")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
+
 
 main()
-from stats import number_of_words
